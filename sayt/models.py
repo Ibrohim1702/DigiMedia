69c3d5ca1cc0a80fc885412909038b5fab33e9bf
Ibrohim1702 <88259165+Ibrohim1702@users.noreply.github.com>
2023-07-05 01:51:41 +0500

    Admin panel
    Admin panel, Views

--- sayt/models.py
+++ sayt/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=128, null=True, blank=True)
+    email = models.CharField(max_length=128, null=True, blank=True)
+    subject = models.CharField(max_length=50, null=True, blank=True)
+    message = models.CharField(max_length=512, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
--- sayt/views.py
+++ sayt/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
+from sayt.models import Contact
+
+
 # Create your views here.
+
+def index(requests):
+    ctx = {}
+    if requests.POST:
+        name = requests.POST.get('name')
+        message = requests.POST.get('message')
+        subject = requests.POST.get('subject')
+        email = requests.POST.get('email')
+        Contact.objects.create(
+            name=name, message=message, subject=subject, email=email
+        )
+
+        ctx = {
+            "contact": index,
+
+        }
+    return render(requests, "homepage_3.html", ctx)
